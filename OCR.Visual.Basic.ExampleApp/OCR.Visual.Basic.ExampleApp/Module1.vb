a1f4e3a941a575642eeddc5552573e340a4b3518
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-06-30 10:33:41 -0700

    Added

--- OCR.Visual.Basic.ExampleApp/OCR.Visual.Basic.ExampleApp/Module1.vb
+++ OCR.Visual.Basic.ExampleApp/OCR.Visual.Basic.ExampleApp/Module1.vb
@@ -1,7 +1,21 @@
-﻿Module Module1
+﻿Imports Cloudmersive.APIClient.NET.OCR.Api
+Imports Cloudmersive.APIClient.NET.OCR.Client
+Imports Cloudmersive.APIClient.NET.OCR.Model
+
+Module Module1
 
     Sub Main()
+        Configuration.Default.AddApiKey("Apikey", "YOUR-API-KEY-HERE")
 
+
+
+        Dim apiInstance = New ImageOcrApi()
+        Dim imageFile = New System.IO.FileStream("C:\\temp\\nytimes.png", System.IO.FileMode.Open)
+        Dim language = "ENG"
+        Dim preprocessing = "Auto"
+
+        Dim result = apiInstance.ImageOcrPost(imageFile, language, preprocessing)
+        Debug.WriteLine(result)
     End Sub
 
 End Module
